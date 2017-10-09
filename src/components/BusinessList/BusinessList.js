45540ad1dc783cd79dcb667e444bf9a2b623da08
Carisa Tong <ctong@shutterstock.com>
2017-10-09 19:44:13 -0400

    first part complete

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,3 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Business.css';
+import './BusinessList.css';
+import Business from '../Business/Business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
