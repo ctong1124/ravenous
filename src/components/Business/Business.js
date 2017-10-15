74cedc13dedef628c58051c1054238a9de5a7f12
Carisa Tong <ctong1124@gmail.com>
2017-10-15 16:46:38 -0400

    end of part 2, not rendering individual business

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Business.css';
 
-let business = {
+let business2 = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
   address: '1010 Paddington Way',
@@ -19,25 +19,50 @@ class Business extends React.Component {
     return (
       <div className="Business">
         <div className="image-container">
-          <img src={business.imageSrc} alt=''/>
+          <img src={business2.imageSrc} alt=''/>
         </div>
-        <h2>{business.name}</h2>
+        <h2>{business2.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
-            <p>{business.address}</p>
-            <p>{business.city}</p>
-            <p>{business.state} {business.zipCode}</p>
+            <p>{business2.address}</p>
+            <p>{business2.city}</p>
+            <p>{business2.state} {business2.zipCode}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{business.category}</h3>
-            <h3 className="rating">{business.rating} stars</h3>
-            <p>{business.reviewCount} reviews</p>
+            <h3>{business2.category}</h3>
+            <h3 className="rating">{business2.rating} stars</h3>
+            <p>{business2.reviewCount} reviews</p>
           </div>
         </div>
       </div>
     )
   }
 };
+
+// class Business extends React.Component {
+//   render() {
+//     return (
+//       <div className="Business">
+//         <div className="image-container">
+//           <img src={this.props.business.imageSrc} alt=''/>
+//         </div>
+//         <h2>{this.props.business.name}</h2>
+//         <div className="Business-information">
+//           <div className="Business-address">
+//             <p>{this.props.business.address}</p>
+//             <p>{this.props.business.city}</p>
+//             <p>{this.props.business.state} {this.props.business.zipCode}</p>
+//           </div>
+//           <div className="Business-reviews">
+//             <h3>{this.props.business.category}</h3>
+//             <h3 className="rating">{this.props.business.rating} stars</h3>
+//             <p>{this.props.business.reviewCount} reviews</p>
+//           </div>
+//         </div>
+//       </div>
+//     )
+//   }
+// };
 
 
 export default Business;
